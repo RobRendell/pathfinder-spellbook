ad13da0155e892d576890556ac835a765a297332
Rob Rendell <rob.rendell.au@gmail.com>
2016-03-10 13:04:47 +1100

    Details panel fully functional.

--- spellbook.js
+++ spellbook.js
@@ -3,6 +3,12 @@ String.prototype.toId = function() {
     return this.replace(/[^a-zA-Z0-9]/g, '_');
 }
 
+String.prototype.toTitleCase = function () {
+    return this.replace(/\w\S*/g, function (match) {
+        return match.charAt(0).toUpperCase() + match.substr(1).toLowerCase();
+    });
+}
+
 var Class = {
     create: function(settings) {
         var newClass = function() {
@@ -187,19 +193,39 @@ var SpellData = Class.create({
     init: function (headings, rawData) {
         this.headings = headings;
         this.rawData = rawData;
-        this.classHeadings = [];
+        this.classNames = { 'wiz': 'Wizard', 'sor': 'Sorcerer'};
         $.each(headings, $.proxy(function (index, value) {
             if ($.inArray(value, this.fixedHeadings) < 0) {
-                this.classHeadings.push(value);
+                if (!this.classNames[value]) {
+                    this.classNames[value] = value.toTitleCase();
+                }
             }
         }, this));
-        console.log('Class headings: ' + this.classHeadings.join(', '));
-        var sources = {};
-        $.each(rawData, function (index, spell) {
-            sources[spell.source] = 1;
-        });
-        this.sources = Object.keys(sources).sort($.proxy(this.sourceSort, this));
-        console.log('Sources: ' + this.sources.join(', '));
+        this.classHeadings = Object.keys(this.classNames).sort()
+        this.classesForSources = {
+            'PFRPG Core': [ 'adept', 'bard', 'cleric', 'druid', 'paladin', 'ranger', 'sor', 'wiz' ],
+            'APG': [ 'alchemist', 'inquisitor', 'oracle', 'summoner', 'witch' ]
+        };
+        $.each(rawData, $.proxy(function (index, spell) {
+            if (!this.classesForSources[spell.source] || $.isArray(this.classesForSources[spell.source])) {
+                $.each(this.classHeadings, $.proxy(function (index, classHeading) {
+                    if (spell[classHeading] == 'NULL') {
+                        spell[classHeading] = null;
+                    } else {
+                        if (!this.classesForSources[spell.source]) {
+                            this.classesForSources[spell.source] = {}
+                        }
+                        this.classesForSources[spell.source][classHeading] = 1
+                    }
+                }, this))
+            }
+        }, this));
+        this.sources = Object.keys(this.classesForSources).sort($.proxy(this.sourceSort, this));
+        $.each(this.sources, $.proxy(function (index, source) {
+            if (!$.isArray(this.classesForSources[source])) {
+                this.classesForSources[source] = Object.keys(this.classesForSources[source]);
+            }
+        }, this));
     },
 
     sourceSort: function (o1, o2) {
@@ -226,6 +252,16 @@ var SpellData = Class.create({
         } else {
             return sourceName;
         }
+    },
+
+    getClassHeadingsForSources: function (sourceList) {
+        var result = {};
+        $.each(sourceList, $.proxy(function (index, source) {
+            $.each(this.classesForSources[source], function (index, classHeading) {
+                result[classHeading] = 1;
+            });
+        }, this));
+        return Object.keys(result);
     }
 
 });
@@ -233,6 +269,8 @@ var SpellData = Class.create({
 var BookKeys = {
     keyBookIDs: 'bookIDs',
     keyBookName: 'bookName',
+    keySelectedSources: 'selectedSources',
+    keySelectedClasses: 'selectedClasses'
 };
 
 var TopMenu = Class.create({
@@ -244,21 +282,32 @@ var TopMenu = Class.create({
         this.spellData = spellData;
         this.bookData = {};
         this.bookMenu = {};
-        var ids = globalSettings.getArray(BookKeys.keyBookIDs);
-        $.each(ids, $.proxy(function (index, id) {
-            this.bookData[id] = new Storage(id);
+        var bookIDs = globalSettings.getArray(BookKeys.keyBookIDs);
+        $.each(bookIDs, $.proxy(function (index, bookID) {
+            this.bookData[bookID] = new Storage(bookID);
         }, this));
         this.refresh();
+        // Create new elements in sub-menus
+        $.each(this.spellData.sources, function (index, source) {
+            var sourceLine = $(`<label><input type="checkbox" id="source_${source.toId()}" name="${source}" class="sourcebook"> ${source}</label>`)
+            $('#sourceItems').append(sourceLine);
+        });
+        $.each(this.spellData.classHeadings, function (index, classHeading) {
+            var className = spellData.classNames[classHeading];
+            var sourceLine = $(`<label class="characterClassLabel"><input type="checkbox" id="class_${classHeading.toId()}" name="${classHeading}" class="characterClass"> ${className}</label>`)
+            $('#classItems').append(sourceLine);
+        });
     },
 
     refresh: function () {
         var topdiv = $('#spellbooks');
-        topdiv.fadeIn();
-        topdiv.html();
+        topdiv.off();
+        topdiv.html('');
         this.addNewBookButton();
         $.each(this.bookData, $.proxy(function (id, storage) {
             this.addBookButton(id, storage);
         }, this));
+        topdiv.fadeIn();
     },
 
     addNewBookButton: function () {
@@ -282,7 +331,7 @@ var TopMenu = Class.create({
     getNewBookID: function () {
         var id;
         do {
-            id = 'book' + Math.random().toFixed(8);
+            id = 'book' + Math.random().toFixed(8).substr(2);
         } while (this.bookData[id]);
         return id;
     },
@@ -292,7 +341,7 @@ var TopMenu = Class.create({
         var button = $('<div class="book"></div>');
         button.attr('id', id);
         button.append($('<img src="book.png" />'));
-        button.append($('<div></div>').text(text));
+        button.append($(`<div class="name_${id}"></div>`).text(text));
         button.on('click touch', $.proxy(this.spellbookClicked, this));
         this.newBookButton.before(button);
     },
@@ -301,53 +350,160 @@ var TopMenu = Class.create({
         var target = evt.currentTarget;
         var id = $(target).attr('id');
         if (!this.bookMenu[id]) {
-            this.bookMenu[id] = new BookMenu(id, this.bookData[id], this.spellData, this.globalSettings);
+            this.bookMenu[id] = new BookMenu(id, this.bookData[id], this.spellData, this.globalSettings, this);
         }
-        this.bookMenu[id].showMenu();
+        this.selectedBook = this.bookMenu[id]
+        this.selectedBook.showMenu();
+    },
+
+    deleteBook: function (id) {
+        this.bookData[id].clearAll();
+        delete(this.bookData[id]);
+        this.globalSettings.set(BookKeys.keyBookIDs, Object.keys(this.bookData));
     }
 
 });
 
 var BookMenu = Class.create({
-    init: function (id, storage, spellData, globalSettings) {
+    init: function (id, storage, spellData, globalSettings, topMenu) {
         this.id = id;
         this.storage = storage;
         this.spellData = spellData;
         this.globalSettings = globalSettings;
+        this.topMenu = topMenu;
         this.currentView = 'menu';
     },
 
     showMenu: function () {
+        $('#bookPanelTitle').removeClass();
+        $('#bookPanelTitle').addClass(`name_${this.id}`)
         var bookName = this.storage.get(BookKeys.keyBookName);
         $('#bookPanelTitle').text(bookName);
         $('#spellbooks').fadeOut();
         $('#bookMenu').fadeIn();
         $('.back').on('click touch', $.proxy(this.back, this));
-        $('#detailsButton').on('click touch', $.proxy(this.detailsPanel, this));
+        $('#detailsButton').on('click touch', $.proxy(this.showDetailsPanel, this));
         this.currentView = 'menu';
+        // Details panel setup
+        $('#detailsAccordion').accordion({
+            collapsible: true,
+            active: false,
+            heightStyle: "content"
+        });
+        $('.sourcebook').on('change', $.proxy(function (evt) {
+            var checkbox = $(evt.target);
+            this.refreshSelectedSources(checkbox.attr('name'), checkbox.prop('checked'));
+        }, this));
+        $('.characterClass').on('change', $.proxy(function (evt) {
+            var checkbox = $(evt.target);
+            this.refreshSelectedClasses(checkbox.attr('name'), checkbox.prop('checked'));
+        }, this));
+        $('#detailsPanelApply').on('click touch', $.proxy(this.onDetailsPanelApply, this));
+        $('#detailsPanelDelete').on('click touch', $.proxy(this.onDetailsPanelDelete, this));
     },
 
     back: function () {
         $('.panel').fadeOut();
         if (this.currentView == 'menu') {
+            $('#detailsAccordion').accordion('destroy');
             $('#book *').off();
-            $('#spellbooks').fadeIn();
+            this.topMenu.refresh();
         } else {
             $('#bookMenu').fadeIn();
             this.currentView = 'menu';
         }
     },
 
-    detailsPanel: function () {
+    showDetailsPanel: function () {
         $('.panel').fadeOut();
         $('#detailsPanel').fadeIn();
         this.currentView = 'detailsPanel';
         $('#spellbookNameInput').val(this.storage.get(BookKeys.keyBookName));
-        $('#detailsAccordion').accordion({
-            collapsible: true,
-            active: false,
-            heightStyle: "content"
+        this.selectedSources = this.storage.getArray(BookKeys.keySelectedSources);
+        this.selectedClasses = this.storage.getArray(BookKeys.keySelectedClasses);
+        this.resetDetailsCheckboxes();
+    },
+
+    resetDetailsCheckboxes: function () {
+        $('.sourcebook').prop('checked', false);
+        $.each(this.selectedSources, function (index, source) {
+            $(`#source_${source.toId()}`).prop('checked', true);
         });
+        this.refreshSelectedSources();
+        $('.characterclass').prop('checked', false);
+        $.each(this.selectedClasses, function (index, characterClass) {
+            $(`#class_${characterClass.toId()}`).prop('checked', true);
+        });
+        this.refreshSelectedClasses();
+    },
+
+    refreshSelectedSources: function (source, enabled) {
+        this.changeSelection(this.selectedSources, source, enabled, $.proxy(this.spellData.sourceSort, this.spellData));
+        if (this.selectedSources.length > 0) {
+            $('#sourceNames').text('Source books: ' + this.selectedSources.join(', '));
+        } else {
+            $('#sourceNames').text('Source books: none selected');
+        }
+        // Only show classes that exist in the given sources, or that are already on
+        var classHeadings = this.spellData.getClassHeadingsForSources(this.selectedSources);
+        $('.characterClassLabel').hide();
+        var showClass = $.proxy(function (index, classHeading) {
+            $(`#class_${classHeading.toId()}`).parent().show();
+        }, this);
+        $.each(classHeadings, showClass);
+        $.each(this.selectedClasses, showClass);
+    },
+
+    refreshSelectedClasses: function (classHeading, enabled) {
+        this.changeSelection(this.selectedClasses, classHeading, enabled, undefined);
+        if (this.selectedClasses.length > 0) {
+            var classNames = this.selectedClasses.map($.proxy(function (key) {
+                return this.spellData.classNames[key];
+            }, this));
+            $('#classNames').text('Character classes: ' + classNames.join(', '));
+        } else {
+            $('#classNames').text('Character classes: none selected');
+        }
+    },
+
+    changeSelection: function (list, value, enabled, sortFn) {
+        if (value) {
+            if (enabled) {
+                list.push(value);
+                list.sort(sortFn);
+            } else {
+                var index = $.inArray(value, list);
+                if (index >= 0) {
+                    list.splice(index, 1);
+                }
+            }
+        }
+    },
+
+    refreshSelection: function (label, element, list, value, enabled, sortFn) {
+        if (list.length > 0) {
+            element.text(label + list.join(', '));
+        } else {
+            element.text(label + 'none selected');
+        }
+    },
+
+    onDetailsPanelApply: function (evt) {
+        var newName = $('#spellbookNameInput').val();
+        this.storage.set(BookKeys.keyBookName, newName);
+        $(`.name_${this.id}`).text(newName);
+        this.storage.set(BookKeys.keySelectedSources, this.selectedSources);
+        this.storage.set(BookKeys.keySelectedClasses, this.selectedClasses);
+        this.back();
+    },
+
+    onDetailsPanelDelete: function (evt) {
+        var name = this.storage.get(BookKeys.keyBookName);
+        if (window.confirm('Do you really want to delete "' + name + '"?  All saved configuration will be lost.')) {
+            this.topMenu.deleteBook(this.id);
+            this.currentView = 'menu';
+            this.back();
+        }
     }
 });
 
@@ -408,5 +564,18 @@ $(document).ready(function () {
     });
     */
     $('.panel').hide();
-    new TopMenu(globalSettings, {});
+    $.csv.toObjects(globalSpellCsv, {}, function (err, spellList) {
+        if (err) {
+            console.error(err);
+        } else {
+            var headingRow = globalSpellCsv.substring(0, globalSpellCsv.search(/[\r\n]/));
+            $.csv.toArray(headingRow, {}, function (err, headings) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    new TopMenu(globalSettings, new SpellData(headings, spellList));
+                }
+            });
+        }
+    });
 });
